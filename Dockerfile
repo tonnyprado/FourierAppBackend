d5dd2ce825a4e13146bffe77089e100ad6d841a9
tonyprado <tonnypradog@hotmail.com>
2025-07-22 10:46:42 -0600

    chore: dockerfile update image and fix cors

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:17-jdk
 WORKDIR /app
-COPY target/dft-0.0.1-SNAPSHOT.jar app.jar
+COPY build/libs/dft-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/tonyprag/DFT/Config/CorsConfig.java
+++ src/main/java/com/tonyprag/DFT/Config/CorsConfig.java
@@ -4,15 +4,21 @@ package com.tonyprag.DFT.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer(){
-        return registry -> registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
